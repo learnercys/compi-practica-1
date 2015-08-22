000a34880aa8e7070d3d98cf03873b8aba3dbe88
Carlos Hernández <learner.cys@gmail.com>
2015-08-22 06:00:36 -0600

    draw polygon

--- app/src/main/java/net/learner/practice/CustomCanvasView.java
+++ app/src/main/java/net/learner/practice/CustomCanvasView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
@@ -22,16 +23,6 @@ public class CustomCanvasView extends View {
 
     public CustomCanvasView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
-
-        /*int x = 10;
-        int y = 10;
-        int width = 300;
-        int height = 50;
-
-        mDrawable = new ShapeDrawable(new OvalShape());
-        mDrawable.getPaint().setColor(0xff74AC23);
-        mDrawable.setBounds(x, y, x + width, y + height);*/
-
     }
 
     protected void onDraw(Canvas canvas) {
@@ -55,7 +46,8 @@ public class CustomCanvasView extends View {
                     return;
 
                 case "polygon":
-                    break;
+                    drawPolygon(canvas);
+                    return;
             }
 
             mDrawable.draw(canvas);
@@ -119,6 +111,60 @@ public class CustomCanvasView extends View {
         Paint paint = new Paint();
         paint.setColor(getColor(shape.getColor()));
         canvas.drawLine(x1, y1, x2, y2, paint);
+    }
+
+    public void drawPolygon( Canvas canvas) {
+
+        ArrayList<Integer> values = shape.getValues();
+
+        int
+                x = values.get(0),
+                y = values.get(1),
+                height = values.get(2),
+                width = values.get(3),
+                sides = values.get(4),
+                radius = height < width ? height / 2 : width / 2;
+
+        Paint paint = new Paint();
+        paint.setColor(getColor(shape.getColor()));
+
+        Path path = new Path();
+
+        float
+                // find centers
+                xCenter = (x + height) / 2,
+                yCenter = (y + width) / 2,
+                fx = 0,
+                fy = 0,
+
+                // find angle between points
+                angle = 360 / sides,
+
+                cAngle = 0;
+
+        for (int side =1; side <= sides; side++) {
+
+            float
+                    cx,
+                    cy;
+
+            if (cAngle == 0) {
+                fx = cx = xCenter + radius; fy = cy = yCenter;
+                path.moveTo(cx, cy);
+            } else {
+                cx = (float)(xCenter + radius * Math.cos(cAngle / 57.2957795));
+                cy = (float)(yCenter - radius * Math.sin(cAngle / 57.2957795));
+                path.lineTo(cx, cy);
+            }
+            System.out.println("cx: " + cx);
+            System.out.println("cy: " + cy);
+            cAngle += angle;
+        }
+
+        // to finish in the started
+        path.lineTo(fx,fy);
+
+        canvas.drawPath(path, paint);
     }
 
     public void setShape(Shape shape) {
